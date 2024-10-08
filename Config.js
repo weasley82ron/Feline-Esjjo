bfe22a3facdd5a212c2407c5f22a77665cff2d9d
Weasley Ron <146439498+weasley82ron@users.noreply.github.com>
2024-10-08 19:51:15 +0530

    Update Config.js

--- Config.js
+++ Config.js
@@ -1,10 +1,10 @@
 const { ButtonStyle } = require("discord.js");
 module.exports = {
-    Token: "MTI4MDMwOTczNDkyNjQ1MDc2MQ.GOJdQj.OkIG9f1hhlmXb6QOp1dr8yj2jrBEO5nwTh9Z6E",
-    Prefix: "?",
+    Token: "Your_Discord_bot_token",
+    Prefix: "",
     Client: {
-        ID: "1280822983471923312",
-        Secret: "JaW5Ia6TA7lKVUZ0LfIDf-v84yJPLwWi",
+        ID: "Discord_Bot_User_ID",
+        Secret: "Discord_Bot_Secret",
     },
     button: {
         grey: ButtonStyle.Secondary,
@@ -14,18 +14,19 @@ module.exports = {
         green: ButtonStyle.Success,
     },
     spotify: {
-        ID: "b5d4c15a99714baaa73e5a370a5bfa09",
-        Secret: "fc4e07486cbd40caa12a62dedf23ee93",
+        ID: "Spotify_ID",
+        Secret: "Spotify_Secret",
     },
     Api: {
-        Topgg: ``,
+        Topgg: `Top.gg_APi_Key`,
     },
-    MongoData: "mongodb+srv://esjjo:cerealgotyou@esjjo.ysu1c.mongodb.net/?retryWrites=true&w=majority&appName=Esjjo",
+    MongoData: "Mongo_DB_URL",
     EmbedColor: "#faeb27",
-    Owners: ["1162754753038667919", "1212431696381612132", "1089552985421520926", "1088061981476532244"],
+    Owners: ["", "", "", ""],
+    // Free Lavalink v3 Versions!!
     Nodes:[
         {
-            name: "node3",
+            name: "Serenetia",
             url: "lavalinkv3-id.serenetia.com:80",
             auth: "BatuManaBisa",
             secure: false,
@@ -39,16 +40,16 @@ module.exports = {
     ],
     hooks: {
         guildAdd:
-            "https://discord.com/api/webhooks/1276790561331740705/K8_YOMb41j_UQmXvasmb-FkJx7ST36B89EtX5Jhv9iFjv7HvYBvC51NXOi3fHwqgATfA",
+            "Discord_Webhook_URL",
         guildRemove:
-            "https://discord.com/api/webhooks/1276790561331740705/K8_YOMb41j_UQmXvasmb-FkJx7ST36B89EtX5Jhv9iFjv7HvYBvC51NXOi3fHwqgATfA",
-        Error: "https://discord.com/api/webhooks/1276790710833643540/FO-ovBmxau1ntGqTDfRCynxgb00kQd-6ZM1AuB_1488cvrtvzlulT9xRgyEwVrx9LbFk",
+            "Discord_Webhook_URL",
+        Error: "Discord_Webhook_URL",
     },
     links: {
-        invite: "https://discord.com/oauth2/authorize?client_id=1280309734926450761",
+        invite: "https://discord.com/oauth2/authorize?client_id=1276175949037305919",
         bg: "https://cdn.discordapp.com/attachments/1265335151207120947/1276824081571909632/20230311_212533_0-00-00-00.png?ex=66caee97&is=66c99d17&hm=72f3f7384f1471d0ad3c206bb864946c12a729df3add02d06ea8b29cd218e689&",
-        support: "https://discord.gg/mizubee",
-        vote: "https://top.gg/bot/1280309734926450761/vote",
+        support: "https://discord.gg/jsk",
+        vote: "https://top.gg/bot/1276175949037305919/vote",
         banner: "https://cdn.discordapp.com/attachments/1276707204585492501/1284121468422062121/Mizu.png?ex=66e57acf&is=66e4294f&hm=f614caf5760ee98cd678715e1d1460c1263515b8fc082861ea197061cac8de6c&",
         spotify:
             "https://cdn.discordapp.com/attachments/1036701023990984724/1038352707720843345/782104564315717642.png",
